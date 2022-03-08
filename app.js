11dd43df035ae58e61120478158dfe03d12de076
Jean Carlo <jeancarlo.castn@gmail.com>
2022-03-08 16:38:44 +0100

    deploy test

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 
 require("./config")(app)
 
+console.log('----- ENV -----', process.env.ORIGIN)
+
 app.use("/api", require("./routes/index.routes"))
 
 require("./error-handling")(app)
