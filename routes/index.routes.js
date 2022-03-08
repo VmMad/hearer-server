eda290ed80f1fb25dda0c7364587fde4b6d43844
Jean Carlo <jeancarlo.castn@gmail.com>
2022-03-08 18:20:48 +0100

    try

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,11 +1,13 @@
 const router = require("express").Router()
 
+const cors = require('cors')
+router.use(cors())
 router.use("/auth", require('./auth.routes'))
 router.use("/profile", require("./profile.routes"))
 router.use("/events", require("./events.routes"))
 router.use("/troubles", require("./troubles.routes"))
 router.use("/contacts", require("./contacts.routes"))
-router.use("/mail",require('./mail.routes'))
+router.use("/mail", require('./mail.routes'))
 router.use("/upload", require("./upload.routes"))
 
 module.exports = router
